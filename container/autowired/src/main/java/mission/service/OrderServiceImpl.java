bf0b67e2fd59b86bf0f5e90265c32d62122417cf
Michael Mair <michael.mair@leftshift.one>
2018-07-25 08:17:42 +0200

    autowired result

--- container/autowired/src/main/java/mission/service/OrderServiceImpl.java
+++ container/autowired/src/main/java/mission/service/OrderServiceImpl.java
@@ -1,11 +1,13 @@
 package mission.service;
 
 import mission.repository.OrderRepository;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Michael Mair
  */
 //todo: #18 annotate with stereotype
+@Service
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
