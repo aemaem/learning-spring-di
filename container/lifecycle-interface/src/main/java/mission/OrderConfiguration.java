275390e85af7a32b68b9e9e1c6772efc8d37be94
Michael Mair <michael.mair@leftshift.one>
2018-10-15 17:39:55 +0200

    bean lifecycle with interfaces

---
+++ container/lifecycle-interface/src/main/java/mission/OrderConfiguration.java
@@ -0,0 +1,13 @@
+package mission;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Michael Mair
+ */
+@Configuration
+//todo: #39 add component scan
+public class OrderConfiguration {
+
+}
---
+++ container/lifecycle-interface/src/test/groovy/mission/service/OrderServiceImplTest.groovy
@@ -0,0 +1,23 @@
+package mission.service
+
+import mission.OrderConfiguration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+import spock.lang.Subject
+
+/**
+ * @author Michael Mair
+ */
+@ContextConfiguration(classes = OrderConfiguration)
+class OrderServiceImplTest extends Specification {
+
+    @Subject
+    //todo: #38 inject bean with annotation
+    private OrderServiceImpl orderService
+
+    def "bean is running"() {
+        expect:
+        orderService.arePropertiesSet()
+    }
+}
